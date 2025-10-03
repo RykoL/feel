386f0d2ccbfaeabd899e70a113cf7a8ff5d1c623
Rico Lang <rico.lang@codecentric.de>
2025-10-03 16:06:37 +0200

    feat: add urls for django allauth

--- feel/urls.py
+++ feel/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('journal/', include('journal.urls'))
+    path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.socialaccount.urls')),
+    path('journal/', include('journal.urls')),
 ]

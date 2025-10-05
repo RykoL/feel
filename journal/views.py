f8b3697516dc6c4bbaf76adae088090560c983a2
Rico Lang <rico.lang@codecentric.de>
2025-10-05 09:29:40 +0200

    feat: return internal server error upon form validation failure

--- journal/views.py
+++ journal/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 
+import logging
 
 from .forms import NewJournalEntryForm
 from .models import Journal
+
+
+logger = logging.getLogger(__name__)
 
 
 # Create your views here.
@@ -24,6 +28,13 @@ def new_journal_entry(request, journal_id):
             entry = form.save()
 
             return redirect("journal_entry_feelings", pk=entry.id)
+        else:
+            # Since the feeling currently is a set of fixed buttons and the journal_id
+            # is infered from the user there is not a lot of feedback we can report back
+            # to the user except for: "Something wen't wrong".
+            logger.error(f"Failed to save JournalEntry: {form.errors}")
+            return render(request, "journal/entry/new_entry.html", context=context, status=500)
+
 
     return render(request, "journal/entry/new_entry.html", context)
 
